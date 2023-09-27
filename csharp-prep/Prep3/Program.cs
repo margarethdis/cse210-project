2de41ce7ecf16df0cb31e036df68e570bb1f277e
margarethdis <margarethdani2021@gmail.com>
2023-09-26 18:46:22 -0600

    k

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,7 +5,7 @@ using System.Security.Cryptography;
 class Program
 {
     static void Main(string[] args)
-    { 
+    {
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
